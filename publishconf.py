97df97a5628f9651b4c55e818923bf14acf33f31
Jeremy Silver <jeremyag@comcast.net>
2021-02-10 19:22:39 -0500

    Removed www from root URL.

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://www.nessiness.com'
+SITEURL = 'https://nessiness.com'
 IMG_DIR = f'{SITEURL}/images'
 SITELOGO = f'{IMG_DIR}/logo.png'
 
