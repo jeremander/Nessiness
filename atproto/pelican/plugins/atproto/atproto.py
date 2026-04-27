69e11121013aa381453eb9ebde6ec1102f9aaf5d
Jeremy Silver <jeremys@nessiness.com>
2026-04-27 19:03:07 -0400

    include date and slug in ATProto rkeys for articles

--- atproto/pelican/plugins/atproto/atproto.py
+++ atproto/pelican/plugins/atproto/atproto.py
@@ -16,7 +16,8 @@ from pelican.writers import Writer
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
 
-ATPROTO_REGISTRY_PATH = Path('atproto_registry.json')
+
+_ATPROTO_REGISTRY = None
 
 
 class TagStripper(HTMLParser):
@@ -32,18 +33,41 @@ class TagStripper(HTMLParser):
         return ''.join(self._parts)
 
 
+def date_to_string(dt: datetime.datetime) -> str:
+    """Converts a datetime to a canonical string for ATProto records."""
+    return dt.astimezone(datetime.timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+
 def strip_tags(html: str) -> str:
     """Strips HTML tags from text."""
     stripper = TagStripper()
     stripper.feed(html)
     return stripper.get_text()
 
+def get_atproto_registry_path(settings: dict[str, Any]) -> Path:
+    path = settings.get('ATPROTO_REGISTRY_PATH')
+    if path is None:
+        raise ValueError('ATPROTO_REGISTRY_PATH is required')
+    return Path(path)
+
+def get_atproto_registry(settings: dict[str, Any]) -> dict[str, Any]:
+    global _ATPROTO_REGISTRY
+    if _ATPROTO_REGISTRY is None:
+        path = get_atproto_registry_path(settings)
+        LOGGER.info(f'Loading ATProto registry from {path}')
+        if path.exists():
+            with open(path) as f:
+                registry = json.load(f)
+        else:
+            registry = {}
+        LOGGER.info(f'{len(registry)} article(s) in registry')
+        _ATPROTO_REGISTRY = registry
+    return _ATPROTO_REGISTRY
 
 def article_to_standard_site_record(settings: dict[str, Any], article: Article) -> dict[str, Any]:
     site_url = settings.get('ATPROTO_SITEURL')
     if site_url is None:
         raise ValueError('ATPROTO_SITEURL is required')
-    date_str = article.date.astimezone(datetime.timezone.utc).isoformat()[:23] + 'Z'
+    date_str = date_to_string(article.date)
     record = {
         'path': '/' + article.slug,
         'site': site_url,
@@ -58,32 +82,33 @@ def article_to_standard_site_record(settings: dict[str, Any], article: Article) 
     }
     return {key: val for (key, val) in record.items() if (val is not None)}
 
+# article_generator_write_article
 
 def update_atproto_records(generator: ArticlesGenerator, writer: Writer) -> None:
     """Updates the local ATProto JSON registry with the current article data."""
+    global _ATPROTO_REGISTRY
     settings = generator.settings
-    atproto_registry_path = settings.get('ATPROTO_REGISTRY_PATH')
-    if atproto_registry_path is None:
-        raise ValueError('ATPROTO_REGISTRY_PATH is required')
-    atproto_registry_path = Path(atproto_registry_path)
-    LOGGER.info(f'Loading ATProto registry from {atproto_registry_path}')
-    if atproto_registry_path.exists():
-        with open(atproto_registry_path) as f:
-            registry = json.load(f)
-    else:
-        registry = {}
-    LOGGER.info(f'{len(registry)} article(s) in registry')
+    registry = get_atproto_registry(settings)
     published_articles = [article for article in generator.articles if (article.status == 'published')]
     # TODO: post the unregistered articles only
     unregistered_articles = [article for article in published_articles if (article.url not in registry)]
     LOGGER.info(f'{len(unregistered_articles)} article(s) are unregistered')
-    registry = {
-        article.url: article_to_standard_site_record(settings, article) for article in published_articles
-    }
+    pub_prefix = settings.get('ATPROTO_PUB_PREFIX')
+    registry = {}
+    for article in published_articles:
+        rkey_segments = [pub_prefix] if pub_prefix else []
+        # for rkey, include the date and slug (hopefully uniquely identifies each article)
+        rkey_segments += [article.date.strftime('%Y%m%d'), article.slug]
+        rkey = ':'.join(rkey_segments)
+        if rkey in registry:
+            raise ValueError(f'duplicate document rkey {rkey!r} in ATProto registry')
+        registry[rkey] = article_to_standard_site_record(settings, article)
+    _ATPROTO_REGISTRY = registry
     LOGGER.info(f'Updated registry with {len(registry)} article(s)')
-    with open(atproto_registry_path, 'w') as f:
+    path = get_atproto_registry_path(settings)
+    with open(path, 'w') as f:
         json.dump(registry, f, indent=2, ensure_ascii=False)
-    LOGGER.info(f'Wrote registry to {atproto_registry_path}')
+    LOGGER.info(f'Wrote registry to {path}')
 
 # TODO: post to Bluesky embedding link to post
 # TODO: upload standard.site record pointing to the post
